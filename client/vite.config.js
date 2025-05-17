405042d1ba969216d825da5c5e65386820c3fd69
Hamzah82 <wokabi108@gmail.com>
2025-05-17 14:52:18 +0700

    First debug

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,20 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Pastikan base path benar
+  build: {
+    outDir: '../dist', // Pastikan output ke folder yang benar
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       '/socket.io': {
         target: 'http://localhost:3001',
-        ws: true
-      }
-    }
-  }
+        ws: true,
+      },
+    },
+  },
+  define: {
+    'process.env': process.env, // Untuk env variables
+  },
 });
